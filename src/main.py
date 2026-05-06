fda7c0d4c1ce33cda285457554f384a65c02883d
DingVersion3 <jakewilk10@gmail.com>
2026-05-06 15:40:21 -0700

    never name something final commit lol

--- src/main.py
+++ src/main.py
@@ -6,5 +6,5 @@ from generate_page import generate_page, generate_pages_recursive
 def main():
     basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
     copy_static("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", "public", basepath)
 main()
