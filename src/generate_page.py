04f2be7165901258e78fe8737e123a0681260b98
DingVersion3 <jakewilk10@gmail.com>
2026-05-15 12:29:23 -0700

    claude fix

--- src/generate_page.py
+++ src/generate_page.py
@@ -23,7 +23,7 @@ def generate_page(from_path, template_path, dest_path, basepath="/"):
 
     page = template.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
     page = page.replace('href="/', f'href="{basepath}')
-    page = page.replace('src="/', f'src="{basepath}')
+    page = page.replace('src="/', f'src="{basepath}images/')
 
     dest_dir = os.path.dirname(dest_path)
     if dest_dir:
